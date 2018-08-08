cd605d35d67481a67030f93472ef68e3ec4b6e85
Serge Polyanskikh <spmathf@gmail.com>
2018-08-08 11:15:52 +0300

    Add week3

---
+++ proj/course3_structure_in_data/week3.py
@@ -0,0 +1,16 @@
+import os
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import utils
+
+
+def header():
+    return 'WEEK 3: Anomaly Detection';
+
+
+def run():
+
+
+    return
